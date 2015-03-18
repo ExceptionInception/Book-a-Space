132cfd1ef661a661a57a210d2e242915e86a947b
Zach <zds5056@psu.edu>
2015-03-17 20:06:01 -0400

    Added db, and initial data loading script

---
+++ data/scripts/initialDataInsert.js
@@ -0,0 +1,259 @@
+db = connect("localhost:27017/bookaspace");
+db.createCollection("workspaces");
+
+db.workspaces.insert({
+	_id: "FP-7-WS-430-189A",
+	type: "cubicle",
+	agency: "Executive Offices",
+	building: "Forum Place",
+	floor: 7,
+	room: "south",
+	size: "6 x 8",
+	amRate: 2.58,
+	pmRate: 2.58,
+	weekly: 25.84,
+	monthly: 516.73,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "FP-7-WS-430-189B",
+	type: "cubicle",
+	agency: "Executive Offices",
+	building: "Forum Place",
+	floor: 7,
+	room: "south",
+	size: "6 x 8",
+	amRate: 2.58,
+	pmRate: 2.58,
+	weekly: 25.84,
+	monthly: 516.73,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "FP-7-WS-430-190A",
+	type: "cubicle",
+	agency: "Executive Offices",
+	building: "Forum Place",
+	floor: 7,
+	room: "south",
+	size: "6 x 8",
+	amRate: 2.58,
+	pmRate: 2.58,
+	weekly: 25.84,
+	monthly: 516.73,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "FP-7-WS-430-190B",
+	type: "cubicle",
+	agency: "Executive Offices",
+	building: "Forum Place",
+	floor: 7,
+	room: "south",
+	size: "6 x 8",
+	amRate: 2.58,
+	pmRate: 2.58,
+	weekly: 25.84,
+	monthly: 516.73,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "FP-7-WS-470-11",
+	type: "cubicle",
+	agency: "Executive Offices",
+	building: "Forum Place",
+	floor: 7,
+	room: "south",
+	size: "6 x 8",
+	amRate: 2.58,
+	pmRate: 2.58,
+	weekly: 25.84,
+	monthly: 516.73,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "FP-7-WS-470-12",
+	type: "cubicle",
+	agency: "Executive Offices",
+	building: "Forum Place",
+	floor: 7,
+	room: "south",
+	size: "6 x 8",
+	amRate: 2.58,
+	pmRate: 2.58,
+	weekly: 25.84,
+	monthly: 516.73,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "FB-506-WS-14",
+	type: "cubicle",
+	agency: "Administration",
+	building: "Finance",
+	floor: 5,
+	room: "506",
+	size: "8 x 8",
+	amRate: 1.27,
+	pmRate: 1.27,
+	weekly: 12.70,
+	monthly: 253.99,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "FB-506-WS-15",
+	type: "cubicle",
+	agency: "Administration",
+	building: "Finance",
+	floor: 5,
+	room: "506",
+	size: "8 x 8",
+	amRate: 1.27,
+	pmRate: 1.27,
+	weekly: 12.70,
+	monthly: 253.99,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "FB-506-WS-16",
+	type: "cubicle",
+	agency: "Administration",
+	building: "Finance",
+	floor: 5,
+	room: "506",
+	size: "8 x 8",
+	amRate: 1.27,
+	pmRate: 1.27,
+	weekly: 12.70,
+	monthly: 253.99,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "KS-7-WS-61",
+	type: "cubicle",
+	agency: "PennDOT",
+	building: "Keystone",
+	floor: 7,
+	room: "west",
+	size: "6 x 8",
+	amRate: 1.90,
+	pmRate: 1.90,
+	weekly: 19.05,
+	monthly: 380.98,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "KS-7-WS-62",
+	type: "cubicle",
+	agency: "PennDOT",
+	building: "Keystone",
+	floor: 7,
+	room: "west",
+	size: "6 x 8",
+	amRate: 1.90,
+	pmRate: 1.90,
+	weekly: 19.05,
+	monthly: 380.98,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "KS-7-WS-63",
+	type: "cubicle",
+	agency: "PennDOT",
+	building: "Keystone",
+	floor: 7,
+	room: "west",
+	size: "6 x 8",
+	amRate: 1.90,
+	pmRate: 1.90,
+	weekly: 19.05,
+	monthly: 380.98,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "KS-7-WS-64",
+	type: "cubicle",
+	agency: "PennDOT",
+	building: "Keystone",
+	floor: 7,
+	room: "west",
+	size: "6 x 8",
+	amRate: 1.90,
+	pmRate: 1.90,
+	weekly: 19.05,
+	monthly: 380.98,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "KS-7-WS-65",
+	type: "cubicle",
+	agency: "PennDOT",
+	building: "Keystone",
+	floor: 7,
+	room: "west",
+	size: "6 x 8",
+	amRate: 1.90,
+	pmRate: 1.90,
+	weekly: 19.05,
+	monthly: 380.98,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "KS-7-WS-66",
+	type: "cubicle",
+	agency: "PennDOT",
+	building: "Keystone",
+	floor: 7,
+	room: "west",
+	size: "6 x 8",
+	amRate: 1.90,
+	pmRate: 1.90,
+	weekly: 19.05,
+	monthly: 380.98,
+	inventory: [],
+	reservations: []
+});
+
+db.workspaces.insert({
+	_id: "KS-7-WS-67",
+	type: "cubicle",
+	agency: "PennDOT",
+	building: "Keystone",
+	floor: 7,
+	room: "west",
+	size: "6 x 8",
+	amRate: 1.90,
+	pmRate: 1.90,
+	weekly: 19.05,
+	monthly: 380.98,
+	inventory: [],
+	reservations: []
+});
+
